460d3fee9c042fbf06599df458cb9d5e86e22faf
Zhixian Xie <zxxie.zhixian@gmail.com>
2024-03-25 17:24:53 -0700

    learned how to draw trajectory

--- utils/Visualize_mj.py
+++ utils/Visualize_mj.py
@@ -36,15 +36,48 @@ if __name__=='__main__':
     print('path',filepath)
     m=mujoco.MjModel.from_xml_path(filepath)
     d=mujoco.MjData(m)
+    print('-----------------------')
+    cam = mujoco.MjvCamera()
+    opt = mujoco.MjvOption()
+
     viewer=mujoco.viewer.launch_passive(m, d)
+    scene=viewer.user_scn
+    scene.maxgeom=10000
+
     #viewer=mujoco.viewer.launch(m, d)
     #viewer=mujoco.viewer.launch()
+    last_pos=None
+    geom_idx=0
     while True:
         #print(mujoco.mj_getState(m,d))
         time.sleep(0.05)
         #d.ctrl[0]=4
         #mujoco.mj_step(m, d)
         mujoco.mj_forward(m, d)
+        #print(viewer.scene)
+        if last_pos is not None:
+            #print(int(mujoco.mjtGeom.mjGEOM_LINE))
+            scene.ngeom+=1
+            mujoco.mjv_initGeom(
+                scene.geoms[geom_idx],
+                type=mujoco.mjtGeom.mjGEOM_LINE,
+                size=np.zeros(3),
+                pos=last_pos,
+                mat=np.eye(3).flatten(),
+                rgba=0.5*np.array([1, 0, 0, 1])
+            )
+            mujoco.mjv_makeConnector(scene.geoms[geom_idx],
+                                 mujoco.mjtGeom.mjGEOM_LINE,
+                                 10,
+                                 d.qpos[0],d.qpos[1],d.qpos[2],
+                                 last_pos[0],last_pos[1],last_pos[2])
+            #viewer.user_scn.geoms[geom_idx].pos[2] = last_pos[2]
+            print(d.qpos[2]-last_pos[2])
+            geom_idx+=1
+            
+            viewer.sync()
+            
         viewer.sync()
         #print(d.ctrl)
-        d.qpos[2]+=0.01
+        last_pos=d.qpos[0:3].copy()
+        d.qpos[2]+=0.02
