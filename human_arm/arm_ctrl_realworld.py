5bfa4d8c7115326a5cb3948932b51a85155f3a4d
Franka Robot <frankarobot.iris@gmail.com>
2024-05-12 14:59:29 -0700

    Add the real-world constraint test code

--- human_arm/arm_ctrl_realworld.py
+++ human_arm/arm_ctrl_realworld.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.abspath(os.getcwd()))
 
 import casadi as cd
 from Envs.robot_arm import EFFECTOR_env_mj, End_Effector_model, DH_to_Mat, Rot_Quat
+from utils.RBF import generate_rbf_quat_z
 from Solvers.OCsolver import  ocsolver_v4
 import numpy as np
 
@@ -17,7 +18,7 @@ from std_msgs.msg import Float64MultiArray
 
 
 LATEST_HOMO_MATRIX=None
-LATEST_THETA_CTRL=np.ones(20)
+LATEST_THETA_CTRL=np.ones(20)#20
 
 #lock 1: for communication with robot
 #lock 2: for communication with learner
@@ -45,13 +46,14 @@ def listener_learner(data):
 
 
 def main():
-    global LATEST_HOMO_MATRIX
+    global LATEST_HOMO_MATRIX, LATEST_THETA_CTRL
     # ros utils
     rospy.init_node('mpc', anonymous=True)
     rospy.Subscriber("robot_otee", Float64MultiArray, listener_robot, queue_size=100)
     rospy.Subscriber("theta", Float64MultiArray, listener_learner, queue_size=100)
 
     vel_pub = rospy.Publisher('velocity_command', Float64MultiArray, queue_size=10)
+    target_pub = rospy.Publisher('target_x', Float64MultiArray, queue_size=10)
     rate = rospy.Rate(50) #hz
 
     #mpc utils
@@ -61,8 +63,17 @@ def main():
     dyn_f = arm_model.get_dyn_f()
     step_cost_vec = np.array([0.8,0.0,30.0,30.0,1.0,0.85]) * 1e0 #param:[kr,kq,kvx,kvy,kvz,kw]
     step_cost_f = arm_model.get_step_cost_param_sep(step_cost_vec)
-    term_cost_vec = np.array([6,8]) * 1e1
+    term_cost_vec = np.array([8,6]) * 1e1
     term_cost_f = arm_model.get_terminal_cost_param(term_cost_vec)
+
+    phi_func =  generate_rbf_quat_z(Horizon,x_center=-0.15,x_half=0.2,ref_axis=np.array([1,0,0]),num_q=10,
+                                z_min=0.1,z_max=1.2, num_z=10, bias=-0.8, epsilon_z=9, epsilon_q=1.8,z_factor=0.05,mode='cumulative')
+    
+    # phi_func =  generate_rbf_quat_z(Horizon,x_center=-0.15,x_half=0.2,ref_axis=np.array([1,0,0]),num_q=10,
+    #                             z_min=0.1,z_max=1.2, num_z=5, bias=-0.8, epsilon_z=7, epsilon_q=1.8,z_factor=0.1,mode='cumulative')
+
+
+    Gamma=1.0 #1.0
 
     controller = ocsolver_v4('arm control')
     controller.set_state_param(7, None, None)
@@ -70,9 +81,10 @@ def main():
     controller.set_dyn(dyn_f)
     controller.set_step_cost(step_cost_f)
     controller.set_term_cost(term_cost_f)
+    controller.set_g(phi_func, gamma=Gamma)
     controller.construct_prob(horizon=Horizon)
 
-    target_end_pos=[-0.6,-0.5,0.5]
+    target_end_pos=[-0.65,-0.5,0.5]
     target_quat=[0.0,-0.707,0.0,0.707]
     target_x=target_end_pos+target_quat
 
@@ -95,11 +107,25 @@ def main():
             #print('pos',latest_pose.flatten())
             #print('quat',latest_quat.flatten())
             curr_mpc_state=np.concatenate((latest_pose,latest_quat),axis=0)
-            vel_command_raw=controller.control(curr_mpc_state,target_x=target_x)
+
+            ros_lock_2.acquire()
+            learned_theta=np.array(LATEST_THETA_CTRL)
+            ros_lock_2.release()
+
+            vel_command_raw = np.zeros(6)
+            try:
+                vel_command_raw=controller.control(curr_mpc_state,weights=learned_theta,target_x=target_x)
+            except:
+                vel_command_raw = np.zeros(6)
+                print('error in control')
+            
             #print(vel_command_raw)
             cmd_msg=Float64MultiArray(data=vel_command_raw)
             #cmd_msg.data=vel_command_raw
             vel_pub.publish(cmd_msg)
+
+        target_msg=Float64MultiArray(data=np.array(target_x))
+        target_pub.publish(target_msg)
             
         rate.sleep()
 
--- human_arm/learner_realworld.py
+++ human_arm/learner_realworld.py
@@ -9,6 +9,9 @@ sys.path.append(os.path.abspath(os.getcwd()))
 import casadi as cd
 from Envs.robot_arm import End_Effector_model,Rot_Quat
 from Solvers.OCsolver import  ocsolver_v4
+from Solvers.Cutter import  cutter_v4
+from Solvers.MVEsolver import mvesolver
+from utils.RBF import generate_rbf_quat_z
 import numpy as np
 
 
@@ -21,43 +24,102 @@ LATEST_HOMO_MATRIX=None
 LATEST_CORR=None
 CORR_FLAG=False
 
-ros_lock=threading.Lock()
+LATEST_TARGET = np.array([-0.65,-0.5,0.5,0.0,-0.707,0.0,0.707])
+#lock 1: correction data
+#lock 2: target position
+ros_lock_1=threading.Lock()
+ros_lock_2=threading.Lock()
 
 def listener_correction(data):
     global LATEST_HOMO_MATRIX,LATEST_CORR,CORR_FLAG
     # lock
     #print(data)
-    ros_lock.acquire()
+    ros_lock_1.acquire()
     LATEST_HOMO_MATRIX=np.array(data.data)[0:16].reshape(4,4).T
     LATEST_CORR=np.array(data.data)[16:22]
     CORR_FLAG=True
     # unlock
-    ros_lock.release()
+    ros_lock_1.release()
 
     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+
+def listener_target(data):
+    global LATEST_TARGET
+    ros_lock_2.acquire()
+    LATEST_TARGET=np.array(data.data)
+    #print('target',LATEST_TARGET)
+    # unlock
+    ros_lock_2.release()
+    
+
     
 def main():
-    global LATEST_THETA,LATEST_HOMO_MATRIX,LATEST_CORR,CORR_FLAG
+    global LATEST_THETA,LATEST_HOMO_MATRIX,LATEST_CORR,CORR_FLAG,LATEST_TARGET
     # ros utils
     rospy.init_node('learner', anonymous=True)
     rospy.Subscriber("human_correction", Float64MultiArray, listener_correction, queue_size=100)
+    rospy.Subscriber("target_x", Float64MultiArray, listener_target, queue_size=100)
 
     theta_pub = rospy.Publisher('theta', Float64MultiArray, queue_size=10)
     rate = rospy.Rate(10) #hz
 
+    # Model
+    dt=0.1
+    Horizon=10
+    arm_model=End_Effector_model(dt=dt)
+    dyn_f = arm_model.get_dyn_f()
+    step_cost_vec = np.array([0.8,0.0,30.0,30.0,1.0,0.85]) * 1e0 #param:[kr,kq,kvx,kvy,kvz,kw]
+    step_cost_f = arm_model.get_step_cost_param_sep(step_cost_vec)
+    term_cost_vec = np.array([8,6]) * 1e1
+    term_cost_f = arm_model.get_terminal_cost_param(term_cost_vec)
+
+    # MPC for trajectory generation
+    phi_func =  generate_rbf_quat_z(Horizon,x_center=-0.15,x_half=0.2,ref_axis=np.array([1,0,0]),num_q=10,
+                                z_min=0.1,z_max=1.2, num_z=10, bias=-0.8, epsilon_z=9, epsilon_q=1.8,z_factor=0.05,mode='cumulative')
+
+    # phi_func =  generate_rbf_quat_z(Horizon,x_center=-0.15,x_half=0.2,ref_axis=np.array([1,0,0]),num_q=10,
+    #                             z_min=0.1,z_max=1.2, num_z=5, bias=-0.8, epsilon_z=7, epsilon_q=1.8,z_factor=0.1,mode='cumulative')
+    
+    Gamma=1.0 #
+
+    controller = ocsolver_v4('learner controller')
+    controller.set_state_param(7, None, None)
+    controller.set_ctrl_param(6, 6*[-1e10], 6*[1e10])
+    controller.set_dyn(dyn_f)
+    controller.set_step_cost(step_cost_f)
+    controller.set_term_cost(term_cost_f)
+    controller.set_g(phi_func, gamma=Gamma)
+    controller.construct_prob(horizon=Horizon)
+
+    # cutter, mvesolveer
+    hb_calculator = cutter_v4('arm cut')
+    hb_calculator.from_controller(controller)
+    hb_calculator.construct_graph(horizon=Horizon)
+
+    theta_dim = 20
+    hypo_lbs = -3 * np.ones(theta_dim)
+    hypo_ubs = 5 * np.ones(theta_dim)
+
+    mve_calc = mvesolver('arm_mve', theta_dim)
+    mve_calc.set_init_constraint(hypo_lbs, hypo_ubs)
+
+    #loop
+
     while not rospy.is_shutdown():
         corr_pose=None
         corr_mat=None
+        correction=None
         process_flag=False
         # lock
-        ros_lock.acquire()
+        ros_lock_1.acquire()
         if CORR_FLAG:
             #print(LATEST_HOMO_MATRIX)
             corr_pose = LATEST_HOMO_MATRIX[0:3,3].reshape(-1,1)
-            corr_mat = LATEST_HOMO_MATRIX[0:3,0:3]
+            corr_mat = np.array(LATEST_HOMO_MATRIX[0:3,0:3])
+            correction = np.array(LATEST_CORR)
             CORR_FLAG=False
             process_flag=True
-        ros_lock.release()
+        ros_lock_1.release()
 
         if process_flag:
             corr_quat=Rot_Quat(corr_mat).full().reshape(-1,1)
@@ -66,9 +128,27 @@ def main():
             corr_state=np.concatenate((corr_pose,corr_quat),axis=0)
 
             # TODO Update Theta
-            LATEST_THETA = np.random.rand(20,1)
+            ros_lock_2.acquire()
+            target_x=np.array(LATEST_TARGET)
+            ros_lock_2.release()
+            
+            controller.control(corr_state, weights=LATEST_THETA, target_x=target_x)
+            correction[0:2]=0
+            correction[2]*=10
+            print('correction',correction)
+            human_corr_e=np.concatenate([correction.reshape(-1, 1), np.zeros((6 * (Horizon - 1), 1))])
+            h, b, h_phi, b_phi = hb_calculator.calc_planes(LATEST_THETA, corr_state, controller.opt_traj,
+                                                                   human_corr=human_corr_e,
+                                                                   target_x=target_x)
+            mve_calc.add_constraint(h, b[0])
+            mve_calc.add_constraint(h_phi, b_phi[0])
+            try:
+                LATEST_THETA, C = mve_calc.solve()
+                print('theta',LATEST_THETA.flatten())
+            except:
+                return -1
 
-        print('dummy theta',LATEST_THETA.flatten())
+        
         theta_msg=Float64MultiArray(data=LATEST_THETA.flatten())
         #cmd_msg.data=vel_command_raw
         theta_pub.publish(theta_msg)
